4028aaeef9d6c9ce6d85b68a0ca0c97c4ef91f0c
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-24 15:37:24 -0600

    Added property table migration file

--- migrations/20170624151713_property.js
+++ migrations/20170624151713_property.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('property', table => {
+    table.increments('property_id').primary();
+    table.text('address').notNull();
+    table.integer('rent_price').notNull();
+    table.text('bedrooms').notNull();
+    table.text('bathrooms').notNull();
+    table.text('square_footage').notNull();
+    table.text('image');
+    table.integer('landlord_id').references('account.id').unsigned().onDelete('cascade')
+    table.integer('location_id').references('location.id').unsigned().onDelete('cascade')
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('property');
 };
