3e178e6cc16ee4ec57a87ff0917ce48f5466a0cb
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-24 15:19:52 -0600

    Installed knex, added migration files

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/athome'
+  },
+  // production: {
+  //   client: 'postgresql',
+  //   connection:
+  // }
+
+};
