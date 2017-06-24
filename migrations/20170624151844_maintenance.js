5670a98d881dbf9135b38fbbfd766e6f5ae5def2
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-24 16:39:12 -0600

    Added maintenace table migration

--- migrations/20170624151844_maintenance.js
+++ migrations/20170624151844_maintenance.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('maintenance', table => {
+    table.increments('id').primary();
+    table.text('title').notNull();
+    table.text('request').notNull();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.boolean('status').defaultTo(false);
+    table.integer('property_id').unsigned().references('id').inTable('property').onDelete('cascade');
+    table.integer('tenant_id').unsigned().references('id').inTable('account').onDelete('cascade');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('maintenance');
 };
