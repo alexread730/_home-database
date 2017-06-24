ce2e20291d864d2695ab677e2c9b8f5ce26eb5e3
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-24 16:41:26 -0600

    Added tenant_property table migration file

--- migrations/20170624151900_tenant_property.js
+++ migrations/20170624151900_tenant_property.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('tenant_property', table => {
+    table.increments('id').primary();
+    table.integer('tenant_id').unsigned().references('id').inTable('account').onDelete('cascade');
+    table.integer('property_id').unsigned().references('id').inTable('property').onDelete('cascade');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('tenant_property');
 };
