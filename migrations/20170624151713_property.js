78ef3cf695a0195512bea14fe9cc7b498352dc45
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-06-24 15:47:29 -0600

    Added documents table migration file

--- migrations/20170624151713_property.js
+++ migrations/20170624151713_property.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('property', table => {
-    table.increments('property_id').primary();
+    table.increments('id').primary();
     table.text('address').notNull();
     table.integer('rent_price').notNull();
     table.text('bedrooms').notNull();
--- migrations/20170624151830_document.js
+++ migrations/20170624151830_document.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('documents', table => {
+    table.increments('id').primary();
+    table.text('title').notNull();
+    table.text('document_url').notNull();
+    table.date('created_on').notNull();
+    table.integer('property_id').references('property.id').unsigned().onDelete('cascade')
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.createTable('documents');
 };
